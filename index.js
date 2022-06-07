6ac81e8188f4cf547f3d78deffad50fdf664c3b6
gideonlangat98 <gideonlangat98@gmail.com>
2022-06-07 21:59:04 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 110);
+console.log(random);
+
+const num3 = 12;
+const num4 = 8;
+let mod = num3%num4;
+console.log(mod);
+
+let max = Math.max(6, 8, 12, 18, 20);
+console.log(max);
